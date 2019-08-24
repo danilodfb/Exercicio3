8f643451129bd499c34d16c3ad8d739575bfbc45
pos liberado <pospos@labcomp.usc>
2019-08-24 14:00:01 -0300

    men

---
+++ src/app/news/news.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {News} from './News';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.page.html',
+  styleUrls: ['./news.page.scss'],
+})
+export class NewsPage implements OnInit {
+
+  private newslist: News[] = [];
+  private news: News;
+  constructor() {
+    
+   }
+
+  enviarAlerta(titulo: string, subtitulo: string){
+    alert(titulo)
+    this.news = new News()
+    this.news.titulo =titulo;
+    this.news.subtitulo = subtitulo;
+    this.newslist.push(this.news);
+  }
+
+  ngOnInit() {
+  }
+
+}
